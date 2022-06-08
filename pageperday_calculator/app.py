8efbd0346044e55f254192075a9332b7a781cf39
Bjells <bauee527@hsd.k12.or.us>
2022-06-08 11:57:32 -0700

    updated app code

--- pageperday_calculator/app.py
+++ pageperday_calculator/app.py
@@ -2,13 +2,15 @@
 import dearpygui.dearpygui as dpg
 
 dpg.create_context()
-dpg.create_viewport(title='Custom Title', width=600, height=300)
+dpg.create_viewport(title='Page per Day Calculator', width=600, height=300)
 
-with dpg.window(label="Page per day calculator", width = 600, height = 300):
-    dpg.add_text("Welcome to the page per day calculator")
+with dpg.window(label="Page per Day Calculator", width = 700, height = 200):
+    dpg.add_text("Welcome to the page per day calculator!")
     dpg.add_input_int(label = "How many pages are there?")
     dpg.add_input_int(label = "How many pages have you read?")
+    dpg.add_input_int(label = "How many days until you have \nto be done reading this book?")
     dpg.add_button(label="Calculate!")
+    dpg.add_text(label = "This is where your results will pop up.")
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
