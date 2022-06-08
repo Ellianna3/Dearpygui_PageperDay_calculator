2bc97a870cf6ea05fa4e426530c86b3b8c05c9ef
Bjells <bauee527@hsd.k12.or.us>
2022-06-08 11:46:53 -0700

    adding primary widgets

--- pageperday_calculator/app.py
+++ pageperday_calculator/app.py
@@ -1,1 +1,16 @@
 #  My DearPyGUI windowed app
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+dpg.create_viewport(title='Custom Title', width=600, height=300)
+
+with dpg.window(label="Page per day calculator", width = 600, height = 300):
+    dpg.add_text("Welcome to the page per day calculator")
+    dpg.add_input_int(label = "How many pages are there?")
+    dpg.add_input_int(label = "How many pages have you read?")
+    dpg.add_button(label="Calculate!")
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
