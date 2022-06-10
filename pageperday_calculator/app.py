151f3accf80f3d0be1e5391c68406d86d9374148
Bjells <bauee527@hsd.k12.or.us>
2022-06-10 11:08:05 -0700

    updated code with getting values

--- pageperday_calculator/app.py
+++ pageperday_calculator/app.py
@@ -4,19 +4,20 @@ import dearpygui.dearpygui as dpg
 dpg.create_context()
 
 #generate ids for all widgets we want to control
-numpages_id = dpg.generate_uuid()
-temp_id = dpg.generate_uuid()
+pages_id = dpg.generate_uuid()
+pagesread_id = dpg.generate_uuid()
 output_id = dpg.generate_uuid()
 
 def callback():
-    print("Number of Pages ID: " + str(dpg.get_value(numpages_id)))
+    output = "Number of Pages Value: " + str(dpg.get_value(pages_id))
+    dpg.set_value(output_id, output)
 
 dpg.create_viewport(title='Page per Day Calculator', width=600, height=300)
 
-with dpg.window(label="Page per Day Calculator", width = 700, height = 200):
+with dpg.window(label="Page per Day Calculator", width = 400, height = 200):
     dpg.add_text("Welcome to the page per day calculator!")
-    dpg.add_input_int(label = "How many pages are there?", width = 100, tag = numpages_id)
-    dpg.add_input_int(label = "How many pages have you read?", width = 100, tag = temp_id)
+    dpg.add_input_int(label = "How many pages are there?", width = 100, tag = pages_id)
+    dpg.add_input_int(label = "How many pages have you read?", width = 100, tag = pagesread_id)
     dpg.add_input_int(label = "How many days until you have \nto be done reading this book?", width = 100)
     dpg.add_button(label="Calculate!", callback = callback)
     dpg.add_text("", tag =output_id)
