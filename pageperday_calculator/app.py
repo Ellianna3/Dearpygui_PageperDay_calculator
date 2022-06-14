6d0ce6be537e46a2473d0e2ef436e2f13f8d0b28
Bjells <bauee527@hsd.k12.or.us>
2022-06-14 11:07:11 -0700

    finalizing pageperday_calculator

--- pageperday_calculator/app.py
+++ pageperday_calculator/app.py
@@ -7,19 +7,36 @@ dpg.create_context()
 pages_id = dpg.generate_uuid()
 pagesread_id = dpg.generate_uuid()
 output_id = dpg.generate_uuid()
+days_id = dpg.generate_uuid()
 
-def callback():
-    output = "Number of Pages Value: " + str(dpg.get_value(pages_id))
+# callback functions
+def get_pagenum():
+    pagenum = dpg.get_value(pages_id)
+    pagesread = dpg.get_value(pagesread_id)
+    days = dpg.get_value(days_id)
+    final = calculate_pages(pagenum, pagesread, days)
+    output = get_output(pagenum, pagesread, days, final)
     dpg.set_value(output_id, output)
+
+def calculate_pages(pn, pr, d):
+    return int((pn - pr) / d)
+
+def get_output(pn, pr, d, f):
+    output = "With {} pages in the book". format(pn)
+    output += "\nand {} pages already read". format(pr)
+    output += "\nand {} days to read the book". format(d)
+    output += "\nyou have to read {} pages a day to finish this \nbook on time!".format(f)
+    return output
+
 
 dpg.create_viewport(title='Page per Day Calculator', width=600, height=300)
 
-with dpg.window(label="Page per Day Calculator", width = 400, height = 200):
+with dpg.window(label="Page per Day Calculator", width = 350, height = 250):
     dpg.add_text("Welcome to the page per day calculator!")
     dpg.add_input_int(label = "How many pages are there?", width = 100, tag = pages_id)
     dpg.add_input_int(label = "How many pages have you read?", width = 100, tag = pagesread_id)
-    dpg.add_input_int(label = "How many days until you have \nto be done reading this book?", width = 100)
-    dpg.add_button(label="Calculate!", callback = callback)
+    dpg.add_input_int(label = "How many days until you have \nto be done reading this book?", width = 100, tag = days_id)
+    dpg.add_button(label="Calculate!", callback = get_pagenum)
     dpg.add_text("", tag =output_id)
 
 dpg.setup_dearpygui()
--- tests/test_pageperday_calculator.py
+++ tests/test_pageperday_calculator.py
@@ -1,5 +1,15 @@
 from pageperday_calculator import __version__
-
+from pageperday_calculator import app
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_calculate_pages_for_small_amount():
+    expected = 5.0
+    results  = app.calculate_pages(20,5,3)
+    assert expected == results
+
+def test_calculate_pages_for_extreme_amount():
+    expected = 100
+    results = app.calculate_pages(300, 100, 2)
+    assert expected == results
